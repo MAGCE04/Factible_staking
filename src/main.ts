013363269345b53035b31607d7c765b953b01e06
MAGCE04 <andre.gejde04@gmail.com>
2025-08-02 03:38:08 -0600

    Deleted main.ts

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-// This is the main entry point for the application
-// Import and export your hooks and components here
-
-export * from './hooks/useAnchorProgram';
-export * from './hooks/useStakingProgram';
-export * from './types/anchor-nft-staking';
