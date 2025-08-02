7d245b8c7bede2afe0012387696adfba02aaef61
MAGCE04 <andre.gejde04@gmail.com>
2025-08-02 03:01:44 -0600

    Fix Buffer polyfill with rollup inject plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { Buffer } from 'buffer'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-globalThis.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
   define: {
     global: 'globalThis',
-    Buffer: 'Buffer',
   },
   resolve: {
     alias: {
