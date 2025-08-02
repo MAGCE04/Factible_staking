ffac80bcec748737562e297bd824848d9ae414af
MAGCE04 <andre.gejde04@gmail.com>
2025-08-02 03:48:51 -0600

    Restore missing React files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { Buffer } from 'buffer'
 (window as any).Buffer = Buffer;
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
